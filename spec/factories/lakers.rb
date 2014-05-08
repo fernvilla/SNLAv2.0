68a559b052a1582ddd3c02172fa48cca471e13dc
fernvilla <fvilla18@gmail.com>
2014-05-08 14:49:23 -0700

    Generate Lakers model

---
+++ spec/factories/lakers.rb
@@ -0,0 +1,13 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :laker do
+    title "MyText"
+    author "MyString"
+    summary "MyText"
+    published "2014-05-08 14:38:24"
+    image "MyString"
+    source "MyString"
+    url "MyText"
+  end
+end
