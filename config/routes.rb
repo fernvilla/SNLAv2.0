01b6855d70dcd6b7e5ad5dc0d6b8fc4f9d46987b
fernvilla <fvilla18@gmail.com>
2014-05-09 14:48:03 -0700

    Install Devise + Social Media Auths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do  
   root 'home#index'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
   resources :lakers
   resources :clippers
   resources :dodgers
