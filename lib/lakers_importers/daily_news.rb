236ec0d090d696db9e2e27df7f26911d5c6e1cd8
fernvilla <fvilla18@gmail.com>
2014-05-11 21:55:47 -0700

    Remove unnecessary loop for single feed news sources]

--- lib/lakers_importers/daily_news.rb
+++ lib/lakers_importers/daily_news.rb
@@ -3,26 +3,22 @@ require 'feedjira'
 class DailyNewsImporter
   def self.import
     source = "Daily News"
-    feeds = [
-      Feedjira::Feed.fetch_and_parse("http://www.dailynews.com/section?template=RSS&profile=4000078&mime=xml")
-    ]
-    
-    feeds.each do |feed|
-      if defined? feed.entries
-        feed.entries.each do |entry|
-          Laker.where(url: entry.url).first_or_create(
-            title:      entry.title,
-            author:     entry.author,
-            summary:    entry.summary,
-            url:        entry.url,
-            published:  entry.published,
-            source:     source,
-          )
-          if defined? entry.image
-            url = Laker.where(url: entry.url).first
-            if !url.image
-              url.update(image: entry.image)
-            end
+    feed = Feedjira::Feed.fetch_and_parse("http://www.dailynews.com/section?template=RSS&profile=4000078&mime=xml")
+
+    if defined? feed.entries
+      feed.entries.each do |entry|
+        Laker.where(url: entry.url).first_or_create(
+          title:      entry.title,
+          author:     entry.author,
+          summary:    entry.summary,
+          url:        entry.url,
+          published:  entry.published,
+          source:     source,
+        )
+        if defined? entry.image
+          url = Laker.where(url: entry.url).first
+          if !url.image
+            url.update(image: entry.image)
           end
         end
       end
--- lib/lakers_importers/oc_register.rb
+++ lib/lakers_importers/oc_register.rb
@@ -3,26 +3,23 @@ require 'feedjira'
 class OCRegisterImporter
   def self.import
     source = "OC Register"
-    feeds = [
-      Feedjira::Feed.fetch_and_parse("http://www.ocregister.com/common/rss/rss.php?catID=18904")
-    ]
+    feed = Feedjira::Feed.fetch_and_parse("http://www.ocregister.com/common/rss/rss.php?catID=18904")
     
-    feeds.each do |feed|
-      if defined? feed.entries
-        feed.entries.each do |entry|
-          Laker.where(url: entry.url).first_or_create(
-            title:      entry.title,
-            author:     entry.author,
-            summary:    entry.summary,
-            url:        entry.url,
-            published:  entry.published,
-            source:     source,
-          )
-          if defined? entry.image
-            url = Laker.where(url: entry.url).first
-            if !url.image
-              url.update(image: entry.image)
-            end
+
+    if defined? feed.entries
+      feed.entries.each do |entry|
+        Laker.where(url: entry.url).first_or_create(
+          title:      entry.title,
+          author:     entry.author,
+          summary:    entry.summary,
+          url:        entry.url,
+          published:  entry.published,
+          source:     source,
+        )
+        if defined? entry.image
+          url = Laker.where(url: entry.url).first
+          if !url.image
+            url.update(image: entry.image)
           end
         end
       end
