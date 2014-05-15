c6dbf7359f6956d6aecbf88359adb13f3fda732b
fernvilla <fvilla18@gmail.com>
2014-05-15 12:09:57 -0700

    Add news source to fron page stories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   def index
     @lakers = Laker.all.limit(10).order('published DESC')
     @clippers = Clipper.all.limit(10).order('published DESC')
+    @dodgers = Dodger.all.limit(10).order('published DESC')
+    @angels = Angel.all.limit(10).order('published DESC')
+    @kings = King.all.limit(10).order('published DESC')
+    @ducks = Duck.all.limit(10).order('published DESC')
+    @chivas = Chiva.all.limit(10).order('published DESC')
+    @galaxy = Galaxy.all.limit(10).order('published DESC')
+    @sparks = Spark.all.limit(10).order('published DESC')
+    @preps = Prep.all.limit(10).order('published DESC')
+    @bruins = Bruin.all.limit(10).order('published DESC')
+    @trojans = Trojan.all.limit(10).order('published DESC')
   end
 end
