cb9514f14385b61d588f94f29970c43a2a7b57f4
fernvilla <fvilla18@gmail.com>
2014-06-08 17:42:06 -0700

    Setup bookmarks controller actions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,33 @@
 class BookmarksController < ApplicationController
   def index
     if current_user
-      # @bookmarks = Bookmark.all
+      @bookmarks = Bookmark.all
+
+      respond_to do |format|
+        format.html
+        format.json { render json: Bookmark.all } 
+      end
     else
       redirect_to new_user_session_path
       flash[:alert] = "Please sign in first"
     end
   end
 
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.user = current_user
+    if @bookmark.save
+      flash[:notice] = "Story has been bookmarked successfully"
+      redirect_to bookmarks_index
+    end
+  end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    flash[:notice] = "Story has been deleted successfully"
+    redirect_to bookmarks_index
+  end
 
   private
     def bookmark_params
